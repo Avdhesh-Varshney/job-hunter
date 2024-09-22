f6d746eb079299c67a08dfb4876e3efa8922eb5f
Avdhesh <114330097+Avdhesh-Varshney@users.noreply.github.com>
2024-09-22 18:07:54 +0530

    bug-fixes

--- streamlit_app.py
+++ streamlit_app.py
@@ -60,7 +60,7 @@ def information(data, service):
   st.subheader("Company/HR Contact Information")
   category = st.radio("Filter data based on:", ['HR', 'Company'])
   if category == 'HR':
-    hr_names = sorted(data["hr_name"].unique())
+    hr_names = sorted(data["hr_name"].astype(str).unique())
     choice = st.selectbox("Select an HR", hr_names)
     raw_data = data[data["hr_name"] == choice]
   else:
@@ -161,11 +161,27 @@ def myStatus(data):
   }
   data_display['status'] = data_display['status'].map(status_color_map)
 
-  filter_status = st.radio("Filter by Status", ['All', 'No Openings', 'Invitation Sent', 'In Talks'])
-  if filter_status != 'All':
-    data_display = data_display[data_display['status'] == status_color_map[filter_status]]
 
-  unique_names = sorted(data_display['hr_name'].unique().tolist() + data_display['company_name'].astype(str).unique().tolist())
+  cols = st.columns(2)
+  with cols[0]:
+    filter_status = st.radio("Filter by Company Status", ['All', 'No Openings', 'Invitation Sent', 'In Talks'])
+    if filter_status != 'All':
+      data_display = data_display[data_display['status'] == status_color_map[filter_status]]
+  with cols[1]:
+    filter_job_status = st.checkbox("Filter by Job Status")
+    if filter_job_status:
+      data_display = data_display[data_display['job_status'] == 'Applied']
+    filter_linkedin_status = st.checkbox("Filter by LinkedIn Status")
+    if filter_linkedin_status:
+      data_display = data_display[data_display['linkedin_status'] == 'Reached Out']
+    filter_twitter_status = st.checkbox("Filter by Twitter Status")
+    if filter_twitter_status:
+      data_display = data_display[data_display['twitter_status'] == 'Reached Out']
+    filter_facebook_status = st.checkbox("Filter by Facebook Status")
+    if filter_facebook_status:
+      data_display = data_display[data_display['facebook_status'] == 'Reached Out']
+
+  unique_names = sorted(data_display['hr_name'].astype(str).unique().tolist() + data_display['company_name'].astype(str).unique().tolist())
   search_term = st.selectbox("Search for Company or HR", ["Select"] + unique_names)
   if search_term != "Select":
     data_display = data_display[data_display.apply(lambda row: (isinstance(row['company_name'], str) and \
@@ -173,7 +189,7 @@ def myStatus(data):
                                                                   (isinstance(row['hr_name'], str) and \
                                                                    search_term in row['hr_name']), axis=1)]
 
-  items_per_page = st.slider("No. of Items per page", min_value=1, max_value=20, step=1, value=10)
+  items_per_page = st.slider("No. of Items per page", min_value=1, max_value=20, step=1, value=1)
   total_items = len(data_display)
   total_pages = (total_items // items_per_page) + 1
 
@@ -182,7 +198,7 @@ def myStatus(data):
     start_idx = (page - 1) * items_per_page
     end_idx = start_idx + items_per_page
 
-    st.dataframe(data_display.iloc[start_idx:end_idx])
+    st.dataframe(data_display[['hr_name', 'company_name']].iloc[start_idx:end_idx])
     st.write(f"#### Summary of Status")
     status_value_counts = data_display['status'].value_counts()
     st.bar_chart(status_value_counts)
